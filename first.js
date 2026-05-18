b10056f65de1a32e655e7697d48fc091d8bc8f07
hrithik112 <hrithikgv5@gmail.com>
2026-05-18 23:46:20 +0530

    first nodeJS code using files

---
+++ first.js
@@ -0,0 +1,11 @@
+const { log } = require('console');
+const fs=require('fs');
+fs.writeFile("output.txt","writing a file",(err)=>{
+    if(err){
+        console.log("error ocuured")
+    }
+    else{
+    console.log("file created succesfully");
+    
+    }
+});
