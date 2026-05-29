25a95257a7ef66b008b31e1e8a6def826adcdd3f
hrithik112 <hrithikgv5@gmail.com>
2026-05-30 00:24:36 +0530

    routing request

--- lecture 4/app.js
+++ lecture 4/app.js
@@ -11,6 +11,20 @@
 
 const http=require('http');
 const server=http.createServer((req,res)=>{
+    if(req.url=='/'){
+        res.setHeader('content-type','text/html');
+    res.write('<html>')
+    res.write('<head><title>sending  reposnse to server</title></head>')
+    res.write('<body><h2> using nodejs for backend </h2></body>')
+    return res.write('</html>')
+    }else if(req.url="notes"){
+         res.setHeader('content-type','text/html');
+    res.write('<html>')
+    res.write('<head><title>sending  reposnse to server</title></head>')
+    res.write('<body><h2> nodejs node 1)intro 2)creating first server 3)sending response 4)routing request </h2></body>')
+    return res.write('</html>')
+
+    }
     res.setHeader('content-type','text/html');
     res.write('<html>')
     res.write('<head><title>sending  reposnse to server</title></head>')
