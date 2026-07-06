611c0e24cf57147d9711b6880ced990b2a374382
hrithik112 <hrithikgv5@gmail.com>
2026-07-07 00:35:54 +0530

    taking user input

--- lecture 3/app.js
+++ lecture 3/app.js
@@ -3,7 +3,7 @@ const server=http.createServer((req,res)=>{
     console.log(req);
     process.exit();
 })
-const PORT=3000
+const PORT=3001;
 server.listen(PORT,()=>{
     console.log(`server running at http://localhost:${PORT}`);
 });
--- lecture 4/app.js
+++ lecture 4/app.js
@@ -9,29 +9,47 @@
 // });
 
 
-const http=require('http');
-const server=http.createServer((req,res)=>{
-    if(req.url=='/'){
-        res.setHeader('content-type','text/html');
-    res.write('<html>')
-    res.write('<head><title>sending  reposnse to server</title></head>')
-    res.write('<body><h2> using nodejs for backend </h2></body>')
-    return res.write('</html>')
-    }else if(req.url="notes"){
-         res.setHeader('content-type','text/html');
-    res.write('<html>')
-    res.write('<head><title>sending  reposnse to server</title></head>')
-    res.write('<body><h2> nodejs node 1)intro 2)creating first server 3)sending response 4)routing request </h2></body>')
-    return res.write('</html>')
+const http = require('http');
+const server = http.createServer((req, res) => {
+    if (req.url == '/') {
+        res.setHeader('content-type', 'text/html');
+        res.write(`<html>
+            <head><title>about</title></head>
+            <body>
+            <form action="/submit-details" method="POST">
+            <input type="text" id="name" name="name" placeholder="enter your name"> <br><br>
+
+            <label for="gender">gender:</label>
+
+            <input type="radio" id="male" name="gender" value="male">
+            <label for="male">male</label>
+            <input type="radio" id="female" name="gender" value="female">
+            <label for="female">female</label><br><br>
+            <input type="submit" value="submit">
+
+            </form>
+            </body>
+        </html>`)
+        return res.end();
+    } else if (req.url === "/notes") {
+        res.setHeader('content-type', 'text/html');
+        res.write('<html>')
+        res.write('<head><title>sending  reposnse to server</title></head>')
+        res.write('<body><h2> nodejs node 1)intro 2)creating first server 3)sending response 4)routing request </h2></body>')
+        res.write('</html>')
+        return res.end();
 
     }
-    res.setHeader('content-type','text/html');
+    res.setHeader('content-type', 'text/html');
     res.write('<html>')
-    res.write('<head><title>sending  reposnse to server</title></head>')
+    res.write('<head><title>node</title></head>')
     res.write('<body><h2> learn node.js </h2></body>')
     res.write('</html>')
+    return res.end();
 })
-const PORT=3000
-server.listen(PORT,()=>{
+const PORT = 3005;
+server.listen(PORT, () => {
     console.log(`server running at http://localhost:${PORT}`);
 });
+
+
