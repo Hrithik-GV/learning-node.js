5f0983e400c4296c799246acd47423cc741e03ee
hrithik112 <hrithikgv5@gmail.com>
2026-07-18 11:52:17 +0530

    practice set for understanding middlewares and handling routes

---
+++ lecture 9-practice/app.js
@@ -0,0 +1,45 @@
+const http=require('http');
+const express=require('express');
+
+
+const app=express();
+
+// app.use((req,res,next)=>{
+//     console.log(`first middleware`,req.url,req.method);
+//     next();
+// })
+// app.use((req,res,next)=>{
+//     console.log(`second middleware`,req.url,req.method);
+//     next();
+// })
+// app.use((req,res,next)=>{
+//     console.log(`third middleware`,req.url,req.method);
+//     res.send(`<h2>third middleware in practice set</h2>`);
+//     next();
+// })
+app.get("/",(req,res,next)=>{
+    console.log(`home middleware`,req.url,req.method);
+    res.send(`<h2>home page<h2>`);
+})
+app.get("/contact-us",(req,res,next)=>{
+    console.log(`contact us middleware`,req.url,req.method);
+    res.send(`
+        <h2>fill the form to contact us<h2>
+        <form action="/contact-us" method="POST">
+        <input type="text" name="name" placeholder="enter your name" >
+        <input type="email" name="email" placeholder="enter your email address">
+        <input type="submit" value="submit">
+        </form>
+        `);
+
+})
+app.post("/contact-us",(req,res,next)=>{
+     console.log(`contact us POST  middleware`,req.url,req.method);
+     res.send(`<h2>we will contact you soon...<h2>`)
+})
+
+const PORT=5001;
+app.listen(PORT,()=>{
+    console.log(`server is running at http://localhost:${PORT}`)
+})
+
