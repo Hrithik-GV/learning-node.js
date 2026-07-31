1356a686c8442a6c797533bbbe8a961576a63d9f
hrithik112 <hrithikgv5@gmail.com>
2026-07-31 23:21:05 +0530

    writing data to files and reading the data from files

--- lecture 13/controllers/error.js
+++ lecture 13/controllers/error.js
@@ -1,3 +1,5 @@
+const path = require("path");
+const rootDir = require("../utils/pathUtils");
 exports.pageNotFound=(req,res,next)=>{
     res.status(404).sendFile(path.join(rootDir,"views","404-page.html"));
 };
--- lecture 13/controllers/homes.js
+++ lecture 13/controllers/homes.js
@@ -1,23 +1,24 @@
- const Home=require('../models/homes')
- exports.getaddHome=(req, res, next) => {
-  res.render("addHome", { Title: "add-Home page" ,currentPage:'addHome'});
+const Home = require("../models/homes");
+
+exports.getaddHome = (req, res, next) => {
+  res.render("addHome", { Title: "add-Home page", currentPage: "addHome" });
 };
 
-
-exports.postaddHome=(req, res, next) => {
+exports.postaddHome = (req, res, next) => {
   console.log(req.body);
 
-  const{houseName,location,price,rating,photo}=req.body;
-  const home=new Home(houseName,location,price,rating,photo);
+  const { houseName, location, price, rating, photo } = req.body;
+  const home = new Home(houseName, location, price, rating, photo);
   home.save();
   res.render("home-added", { Title: "home-added page" });
 };
 
-exports.getHome=(req,res,next)=>{
-
-  const registeredHome=Home.fetchAll()
-  console.log(registeredHome);
-
-res.render('home',{registeredHome :registeredHome,Title:'Home Page',currentPage:'Home'});
+exports.getHome = (req, res, next) => {
+  Home.fetchAll((registeredHome) => {
+    res.render("home", {
+      registeredHome: registeredHome,
+      Title: "Home Page",
+      currentPage: "Home",
+    });
+  });
 };
-
