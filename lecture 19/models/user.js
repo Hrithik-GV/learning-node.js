d1762d3b4be84fc286ca0da0dcf65683d908bd86
hrithik112 <hrithikgv5@gmail.com>
2026-08-17 23:02:24 +0530

    adding user model

---
+++ lecture 19/models/user.js
@@ -0,0 +1,30 @@
+const mongoose=require('mongoose');
+
+
+const userSchema =mongoose.Schema({
+    firstName:{
+        type:String,
+        required :[true,'first name is required']
+    },
+    lastName:{
+        type:String
+    },
+    email:{
+        type:String,
+        required:[true,'email is required'],
+        unique:true
+    },
+    password:{
+        type:String,
+        required:[true,'password is required']
+    },
+    userType:{ 
+        type:String,
+        enum:['guest','host'],
+        default:'guest'
+    }
+
+})
+
+
+module.exports=mongoose.model('User', userSchema);
